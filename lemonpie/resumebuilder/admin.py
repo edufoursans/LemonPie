1acfd7b23aa8fdd6139b163f08c011b1bf6f63b5
lshao <laetitia.shao@gmail.com>
2017-07-10 03:49:07 +0200

    Adding new models

--- lemonpie/resumebuilder/admin.py
+++ lemonpie/resumebuilder/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
+from .models import (
+  CVGeneral,
+  CVEntry,
+  GroupEntry,
+  ActivityEntry,
+  WorkEntry,
+  EducationEntry,
+  SkillEntry,
+  PersonalEntry,
+  HobbyEntry,
+  CVGeneralGroupEntryPairing,
+  CVEntryGroupEntryPairing
+)
+
 # Register your models here.
+admin.site.register(CVGeneral)
+admin.site.register(CVEntry)
+admin.site.register(GroupEntry)
+admin.site.register(ActivityEntry)
+admin.site.register(WorkEntry)
+admin.site.register(EducationEntry)
+admin.site.register(SkillEntry)
+admin.site.register(PersonalEntry)
+admin.site.register(HobbyEntry)
+admin.site.register(CVGeneralGroupEntryPairing)
+admin.site.register(CVEntryGroupEntryPairing)
