c58cc4b97d66203f2a0b55c92fae1f0a576b669e
lshao <laetitia.shao@gmail.com>
2017-07-17 17:55:48 -0700

    Add empty test for models

--- lemonpie/resumebuilder/tests.py
+++ lemonpie/resumebuilder/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
 
 # Create your tests here.
+from .models import (
+  CVGeneral,
+  CVEntry,
+  GroupEntry,
+  ActivityEntry,
+  WorkEntry,
+  EducationEntry,
+  SkillEntry,
+  PersonalEntry,
+  HobbyEntry,
+  CVGeneralGroupEntryPairing,
+  CVEntryGroupEntryPairing
+)
+
+
+class CVModelTest(TestCase):
+
+    def vacuous_test(self):
+        """
+        This is a vacuous test
+        """
+        self.assertTrue(True)
