ffb3c0186fbf04ecbb128cc95a91090dcc3f2e7a
lshao <laetitia.shao@gmail.com>
2017-07-24 21:23:32 -0700

    minimal view of CVs

--- lemonpie/resumebuilder/urls.py
+++ lemonpie/resumebuilder/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<cv_id>[0-9]+)/$', views.cv_view, name='cv_view'),
 ]
--- lemonpie/resumebuilder/views.py
+++ lemonpie/resumebuilder/views.py
@@ -1,5 +1,26 @@
-from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 
+from .models import CVGeneral
+from .models import CVGeneralGroupEntryPairing, CVEntryGroupEntryPairing
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the resume builder page.")
+    all_cv_list = CVGeneral.objects.all()
+    context = {
+        'cv_general_list': all_cv_list
+    }
+    return render(request, 'resumebuilder/index.html', context)
+
+
+def cv_view(request, cv_id):
+    cv_general = get_object_or_404(CVGeneral, pk=cv_id)
+    cvgrouppairing = CVGeneralGroupEntryPairing.objects.filter(cv_general__id=cv_id)
+    group_entries = [cvgroup.group_entry.id for cvgroup in cvgrouppairing]
+    entrygrouppairing = CVEntryGroupEntryPairing.objects.filter(
+        group_entry__id__in=group_entries)
+    context = {
+        'cv_general': cv_general,
+        'cvgrouppairing': cvgrouppairing,
+        'entrygrouppairing': entrygrouppairing,
+    }
+    return render(request, 'resumebuilder/details.html', context)
