2e7d5449d0b226209fd7d68ff64de91f76b208d3
lshao <laetitia.shao@gmail.com>
2017-07-25 17:47:51 -0700

    namespacing the urls

--- lemonpie/resumebuilder/urls.py
+++ lemonpie/resumebuilder/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'resumebuilder'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<cv_id>[0-9]+)/$', views.cv_view, name='cv_view'),
